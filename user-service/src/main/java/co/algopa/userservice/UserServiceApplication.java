1b99456a0342f03498136323aa88e99d60bda00d
Byeongju, Shin <cd80@kakao.com>
2022-01-12 09:51:27 +0900

    init: 프로젝트 생성

---
+++ user-service/src/main/java/co/algopa/userservice/UserServiceApplication.java
@@ -0,0 +1,11 @@
+package co.algopa.userservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+  public static void main(String[] args) {
+
+  }
+}
